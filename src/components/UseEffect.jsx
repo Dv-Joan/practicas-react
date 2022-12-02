abfc1c504696b213cb1738005d7b1d99167b099c
Dv-Joan <joan300501@gmail.com>
2022-12-02 18:03:07 -0500

    Practicing Fetch APIs

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -1,19 +1,51 @@
 import { useState, useEffect } from "react";
+import Loader from "./Loader";
+
+const acces_token =
+  "6eb7450739c638cf7c2f3787fa92a123b202e2bd489ff73d114ce7416cf4a10bf95667d96398db52714d1b2f03b84b0c93688c439812841544d4b57d71920be9";
 
 function UseEffect() {
   const [name, setName] = useState("");
-  const handleChange = (e) => {
-    setName(e.target.value);
+  const [images, setImages] = useState([]);
+  const [artworks, setArtworks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hola ${name}`);
   };
 
+  const getArtData = async () => {
+    const response = await fetch(
+      "https://v1.api.amethyste.moe/image/wallpaper",
+
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${acces_token}`,
+        },
+      }
+    );
+    const imagesData = await response.json();
+    setImages(imagesData);
+  };
+  const getArtworksData = async () => {
+    const artWorksresponse = await fetch(
+      "https://api.artic.edu/api/v1/artworks/"
+    );
+    const artworksData = await artWorksresponse.json();
+
+    setArtworks(artworksData.data);
+  };
+
+  useEffect(() => {
+    getArtworksData();
+    getArtData();
+  }, []);
   return (
     <div>
       <h1>useEffect</h1>
-      <form
-        onSubmit={() => {
-          alert(`Hola ${name}`);
-        }}
-      >
+      <form onSubmit={handelSubmit}>
         <label htmlFor="name" className="text-gray-500 mr-3 ">
           Name
         </label>
@@ -22,7 +54,8 @@ function UseEffect() {
           type="text"
           name="name"
           id="name"
-          onChange={handleChange}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <button
           type="submit"
@@ -31,7 +64,44 @@ function UseEffect() {
           Submit
         </button>
       </form>
-      <p>{name}</p>
+      <div className="mt-4">
+        <h3 className="text-2xl">
+          Fetch RandomImage Status :
+          <span className="bg-green-500 text-white px-3 mx-1 rounded">
+            {images.status}
+          </span>
+        </h3>
+        {loading ? (
+          <Loader />
+        ) : (
+          <img src={images} alt="random image" className="w-1/2" />
+        )}
+      </div>
+      <div className="mt-4">
+        <h3 className="text-2xl">
+          Fetch ArtWork Status :
+          <span className="bg-green-500 text-white px-3 mx-1 rounded">
+            Great Success
+          </span>
+        </h3>
+        {loading ? (
+          <Loader />
+        ) : (
+          <ul className="text-left">
+            {artworks.map((artwork) => (
+              <li className="mb-10" key={artwork.id}>
+                <h2>Title: {artwork.title}</h2>
+                <h3>Origin : {artwork.place_of_origin}</h3>
+                {/*  <img
+                  src={artwork.thumbnail.lqip}
+                  alt={artwork.thumbnail.alt_text}
+                /> */}
+                <hr />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
